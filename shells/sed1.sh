9054b928151bf405290c17869bb52dc0ef116497
moqimoqidea <moqimoqidea@gmail.com>
2022-11-02 23:28:08 +0800

    Update sed1.sh

--- shells/sed1.sh
+++ shells/sed1.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+echo -e "sed 's/dog/cat/' data1.txt:"
 sed 's/dog/cat/' data1.txt
+
+echo -e "\n"
+echo -e "sed 's/dog/cat/; s/brown/red/' data1.txt:"
+sed 's/dog/cat/; s/brown/red/' data1.txt
