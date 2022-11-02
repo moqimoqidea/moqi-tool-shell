712f4cc86678d02f4ceff5893003a79079316daf
moqimoqidea <moqimoqidea@gmail.com>
2022-11-02 23:35:46 +0800

    update gawk1.sh

--- shells/gawk1.sh
+++ shells/gawk1.sh
@@ -7,3 +7,7 @@ echo -e "\n"
 echo -e "gawk -F\; '{print $3}' data3.txt"
 gawk -F\; '{print $3}' data3.txt
 
+echo -e "\n"
+echo -e "gawk 3: My name is Christine"
+echo "My name is Rich" | gawk '{$4="Christine"; print $0}'
+
