d0e32e8ed1e0174f4536e9d6181ad097a9a40d39
moqimoqidea <moqimoqidea@gmail.com>
2022-11-03 22:54:38 +0800

    Update sed2.sh

---
+++ shells/sed2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo -e "sed 's/test/trial/' data4.txt"
+sed 's/test/trial/' data4.txt
+
+echo -e "\n"
+echo -e "sed 's/test/trial/2' data4.txt"
+sed 's/test/trial/2' data4.txt
+
+echo -e "\n"
+echo -e "sed 's/test/trial/g' data4.txt"
+sed 's/test/trial/g' data4.txt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
