39eee1ed671cfd9216822d7602a545fffa17870f
moqimoqidea <moqimoqidea@gmail.com>
2022-11-03 23:22:31 +0800

    Update sed2.sh +1

--- shells/sed2.sh
+++ shells/sed2.sh
@@ -35,8 +35,26 @@ echo -e "\n"
 echo -e "sed '2,$s/dog/cat/' data1.txt"
 sed '2,$s/dog/cat/' data1.txt
 
+echo -e "\n"
+echo -e "sed '/rich/s/bash/csh/' etc_password.txt | tail -1"
+sed '/rich/s/bash/csh/' etc_password.txt | tail -1
 
+echo -e "\n"
+echo -e "sed '3d' data6.txt"
+sed '3d' data6.txt
 
+echo -e "\n"
+echo -e "sed '2,3d' data6.txt"
+sed '2,3d' data6.txt
+
+echo -e "\n"
+echo -e "sed '2,$d' data6.txt"
+sed '2,$d' data6.txt
+
+# https://stackoverflow.com/a/339941
+echo -e "\n"
+echo -e "tail -n +2 data6.txt"
+tail -n +2 data6.txt
 
 
 
