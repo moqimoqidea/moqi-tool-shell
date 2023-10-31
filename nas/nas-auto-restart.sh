f1488bd8ef4f1db25e7c57fde51ce108211e47a6
moqimoqidea <moqimoqidea@gmail.com>
2023-10-31 22:54:24 +0800

    updata nas auto restart

--- nas/nas-auto-restart.sh
+++ nas/nas-auto-restart.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 
-# source: https://post.smzdm.com/p/aoowpxq7/
-# after gpt tuning
-
 # Configuration
-router_ip="192.168.1.1"
 check_interval=60
-max_failures=10
+min_failures=10
+min_successes=10
 
-opt_dir="/var/services/homes/moqi/shell/auto-shutdown"
+opt_dir="/var/services/homes/moqi/shell/auto-opt"
 
 switch_txt="$opt_dir/switch.txt"
+ping_address_txt="$opt_dir/ping_address.txt"
 
-# Initialize failure count
+# Initialize failure and success counts
 failure_count=0
+success_count=0
 
 # Get today's date in YYYYMMDD format
 today=$(date +%Y%m%d)
@@ -22,9 +21,18 @@ today=$(date +%Y%m%d)
 ping_log="$opt_dir/ping-$today.log"
 result_log="$opt_dir/result-$today.log"
 
+# Function to get current time
+get_current_time() {
+    echo $(date +%Y-%m-%d\ %H:%M:%S)
+}
+
 # Log the start time
-current_time=$(date +%Y/%m/%d/%H:%M:%S)
+current_time=$(get_current_time)
 echo "$current_time Starting the script" >> "$result_log"
+
+# Read the first line from the ping address file
+router_ip=$(head -n 1 "$ping_address_txt")
+echo "Ping address is $router_ip" >> "$result_log"
 
 # Read each line from the switch file
 while IFS= read -r switch_status; do
@@ -36,7 +44,7 @@ while IFS= read -r switch_status; do
         new_day=$(date +%Y%m%d)
         if [ "$new_day" != "$today" ]; then
             # Log a message indicating the end of the day's execution
-            current_time=$(date +%Y/%m/%d/%H:%M:%S)
+            current_time=$(get_current_time)
             echo "$current_time Execution for today ($today) is complete. Exiting script." >> "$result_log"
 
             # Exit the script
@@ -46,21 +54,27 @@ while IFS= read -r switch_status; do
         # Ping the router
         if ping -c 4 "$router_ip" > "$ping_log"; then
             # Ping succeeded
-            current_time=$(date +%Y/%m/%d/%H:%M:%S)
-            echo "$current_time Ping succeeded" >> "$result_log"
+            current_time=$(get_current_time)
+            echo "$current_time Ping to $router_ip succeeded" >> "$result_log"
+            ((success_count++))
             failure_count=0
         else
             # Ping failed
             ((failure_count++))
-            current_time=$(date +%Y/%m/%d/%H:%M:%S)
-            echo "$current_time Ping failed - Attempt $failure_count" >> "$result_log"
+            current_time=$(get_current_time)
+            echo "$current_time Ping to $router_ip failed - Attempt $failure_count" >> "$result_log"
+            success_count=0
+        fi
 
-            # Check if the failure count has reached the limit
-            if [ "$failure_count" -gt "$max_failures" ]; then
-                echo "$current_time Maximum failures reached - Restarting NAS" >> "$result_log"
-                # Restart NAS
-                synoshutdown --reboot
-            fi
+        current_time=$(get_current_time)
+        echo "$current_time failure_count is $failure_count, success_count is $success_count" >> "$result_log"
+        echo "" >> "$result_log"
+
+        # Check if the failure count has reached the limit and success count has reached the minimum
+        if [ "$failure_count" -ge "$min_failures" ] && [ "$success_count" -ge "$min_successes" ]; then
+            echo "$current_time Conditions met for NAS restart" >> "$result_log"
+            # Restart NAS
+            # synoshutdown --reboot
         fi
 
         # Wait for the specified interval before the next check
